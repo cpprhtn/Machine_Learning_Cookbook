5fb15d3568d485bb4b8d2f7615f6761eed0d86a7
Lee_JunWon <cpprhtn@naver.com>
2020-08-13 22:08:38 +0900

    8/13

---
+++ Chapter7/7_시차특성 만들기.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 22:02:33 2020
+
+@author: cpprhtn
+"""
+
+
+import pandas as pd
+
+dataframe = pd.DataFrame()
+
+dataframe["dates"] = pd.date_range("1/1/2001", periods=5, freq="D")
+dataframe["stock_price"] = [1.1,2.2,3.3,4.4,5.5]
+
+#한 행 뒤의 값을 가져옵니다.
+dataframe["previous_days_stock_price"] = dataframe["stock_price"].shift(1)
+
+dataframe
