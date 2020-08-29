e39536e9b10711b3e37dd2d5ad1fe91eff34010a
Lee_JunWon <cpprhtn@naver.com>
2020-08-29 20:46:28 +0900

    8/29

---
+++ Chapter11/2_기본 회귀 모델.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 29 20:01:41 2020
+
+@author: cpprhtn
+"""
+
+
+
+from sklearn.datasets import load_boston
+from sklearn.dummy import DummyRegressor
+from sklearn.model_selection import train_test_split
+
+boston = load_boston()
+
+features, target = boston.data, boston.target
+
+features_train, features_test, target_train, target_test = train_test_split(
+    features, target, random_state=0)
+
+#더미 회귀 모델
+dummy = DummyRegressor(strategy='mean')
+
+dummy.fit(features_train, target_train)
+
+#R^2 점수
+dummy.score(features_test, target_test)
+
+
+
+
+from sklearn.linear_model import LinearRegression
+
+#간단한 선형 회귀 모델
+ols = LinearRegression()
+ols.fit(features_train, target_train)
+
+#R^2 점수
+ols.score(features_test, target_test)
+
+
+#20으로 예측하는 더미 회귀 모델
+clf = DummyRegressor(strategy='constant', constant=20)
+clf.fit(features_train, target_train)
+
+#점수
+clf.score(features_test, target_test)
